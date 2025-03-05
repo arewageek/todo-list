bfcd1b82ee70191ff0f79ff2739588117c6c2cd2
Ehi <ehigift4@gmail.com>
2025-03-05 11:36:17 +0100

    create auth module

--- routes/auth.php
+++ routes/auth.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use Modules\Auth\App\Http\Controllers\Auth\AuthenticatedSessionController;
+use Modules\Auth\App\Http\Controllers\Auth\ConfirmablePasswordController;
+use Modules\Auth\App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use Modules\Auth\App\Http\Controllers\Auth\EmailVerificationPromptController;
+use Modules\Auth\App\Http\Controllers\Auth\NewPasswordController;
+use Modules\Auth\App\Http\Controllers\Auth\PasswordController;
+use Modules\Auth\App\Http\Controllers\Auth\PasswordResetLinkController;
+use Modules\Auth\App\Http\Controllers\Auth\RegisteredUserController;
+use Modules\Auth\App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
