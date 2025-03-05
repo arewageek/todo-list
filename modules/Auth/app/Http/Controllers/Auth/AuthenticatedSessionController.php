ad3e6a6bb30763d50ae52cc1947b82a801d34a6a
Ehi <ehigift4@gmail.com>
2025-03-05 13:40:57 +0100

    Modified Authenticated section controller namespace

--- modules/Auth/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ modules/Auth/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,7 @@
 namespace Modules\Auth\App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use Modules\Auth\App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
