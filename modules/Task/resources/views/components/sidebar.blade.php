c4aa8399bebe5d7dbecbf95f73daae4dfb253bdb
otis <otisolekwu2017@gmail.com>
2025-03-13 10:25:07 +0100

    create page layout

---
+++ modules/Task/resources/views/components/sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="h-screen w-[20%] bg-[#295d66] px-5 py-10">
+    <a href="/my-day" class="text-white">
+      <div class="flex justify-center items-center gap-3 text-xl font-bold">
+        <i class="fa-solid fa-gauge"></i>
+        My day
+        <span class="text-[9pt] border-2 border-white h-[20pt] w-[18pt] rounded-full flex justify-center items-center">1</span>
+      </div>
+    </a>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,17 +10,18 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-[#043e4a]">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
@@ -28,7 +29,9 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
+            <main class="flex flex-col lg:flex-row">
+                <x-task::sidebar/>
+                <div class="w-full lg:[20%] ">
                 {{ $slot }}
             </main>
         </div>
