440af2f36a820a4a86c379051ffa1f24f6bf537e
Timothy Eldon <timothyeldon93@gmail.com>
2025-03-17 10:01:47 +0100

    Upadate the task list

--- modules/Task/resources/views/livewire/task-list.blade.php
+++ modules/Task/resources/views/livewire/task-list.blade.php
@@ -1,24 +1,46 @@
-<div>
-    @if (session()->has('message'))
+
+
+<div class="w-100 bg-teal py-5 px-5 d-flex flex-column justify-content-between">
+    <div>
+
+        @foreach ($tasks as $task)
+            <div class="d-flex justify-content-between bg-light-transparent p-3 my-2">
+                <div class="w-100 container d-flex gap-2">
+                    <div>
+                        <div class="avatar"></div>
+                    </div>
+        
+        
+                    <div class="text-white">
+                        <div class="d-flex gap-2 mb-2">
+                            <i class="fa-solid fa-lock"></i>
+                            <h4>my list>personal</h4>
+                        </div>
+                        <h3 class="fw-bold fs-4">{{ $task->title }}</h3>    
+                    </div>
+    
+    
+                </div>
+                <button wire:click="deleteTask({{ $task->id }})">Delete</button>
+            </div>
+        @endforeach
+    </div>
+
+    <div>
+        @if (session()->has('message'))
         <div style="color: green;">{{ session('message') }}</div>
     @endif
 
     @if (session()->has('error'))
         <div style="color: red;">{{ session('error') }}</div>
     @endif
-
-    <form wire:submit="createTask" method="post">
-        <input type="text" name="title" wire:model="title" placeholder="Enter task title">
-        <textarea type="text" rows="4" name="description" wire:model="description" placeholder="Enter task description"></textarea>
-        <button wire:click="createTask">Add Task</button>
-    </form>
-
-    <ul>
-        @foreach ($tasks as $task)
-            <li>
-                {{ $task->title }} - {{ $task->description }}
-                <button wire:click="deleteTask({{ $task->id }})">Delete</button>
-            </li>
-        @endforeach
-    </ul>
+        <div class="bg-light-transparent px-1 py-1 mt-1 mt-3 text-white">
+            <form wire:submit.prevent="createTask" class="d-flex">
+                <button class="btn p-0 fs-4">
+                    <i class="fa-solid fa-square-plus text-white"></i>
+                </button> 
+                <input wire:model="title" type="text" placeholder="Add task" class="create-task-form bg-transparent w-100 ">
+            </form>
+        </div>
+    </div>
 </div>
