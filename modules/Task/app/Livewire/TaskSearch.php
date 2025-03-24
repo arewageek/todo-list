edbf020252ddf78adf7de28a51fb1d2c14ec3164
Nathaniel <egbodonathaniel@yahoo.com>
2025-03-24 11:35:45 +0100

    create livewire task search component

---
+++ modules/Task/app/Livewire/TaskSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Task\App\Livewire;
+
+use Livewire\Component;
+
+class TaskSearch extends Component
+{
+    public function render()
+    {
+        return view('task::livewire.task-search');
+    }
+}
