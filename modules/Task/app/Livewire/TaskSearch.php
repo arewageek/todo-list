645f9dc4727ad5180ab19cfa45cc7886c43df240
Nathaniel <egbodonathaniel@yahoo.com>
2025-03-24 13:33:25 +0100

    integrate task search backend

--- modules/Task/app/Livewire/TaskSearch.php
+++ modules/Task/app/Livewire/TaskSearch.php
@@ -2,12 +2,50 @@
 
 namespace Modules\Task\App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Modules\Task\App\Models\Task;
 
 class TaskSearch extends Component
 {
+
+
+    public $search = '';
+    public $tasks = [];
+
+    public function mount(){
+        $this->fetchAuthTasks();
+    }
+
+    public function updated($propertyName)
+    {
+        
+        if ($propertyName === 'search') {
+            dd('searching');
+            $this->fetchAuthTasks(); 
+        }
+    }
+
+    private function fetchAuthTasks()
+    {
+        if (Auth::check()) {
+            $query = Task::where('user_id', Auth::id());
+
+            if (!empty($this->search)) {
+                $query->where(function ($q) {
+                    $q->where('title', 'like', "%{$this->search}%")
+                      ->orWhere('description', 'like', "%{$this->search}%");
+                });
+            }
+
+            $this->tasks = $query->get();
+        } else {
+            $this->tasks = [];
+        }
+    }
+
     public function render()
     {
-        return view('task::livewire.task-search');
+        return view('task::livewire.task-search', ['tasks'=> $this->tasks]);
     }
 }
--- modules/Task/resources/views/livewire/task-search.blade.php
+++ modules/Task/resources/views/livewire/task-search.blade.php
@@ -1,3 +1,12 @@
 <div>
-    <h3>The <code>TaskSearch</code> livewire component is loaded from the <code>Task</code> module.</h3>
+    <input type="text" wire:model.debounce.500ms="search" placeholder="Search tasks..."
+        class="border p-2 rounded w-full">
+
+    <ul>
+        @forelse ($tasks as $task)
+            <li class="border-b py-2">{{ $task->title }} - {{ $task->description }}</li>
+        @empty
+            <li>No tasks found.</li>
+        @endforelse
+    </ul>
 </div>
